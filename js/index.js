e46598dc0702cdabcb7b5536b832abdcc83ebd24
Oksana M <kesusha.melnyk@gmail.com>
2022-04-28 23:39:57 -0700

    Add CSS styles

--- js/index.js
+++ js/index.js
@@ -5,17 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const footer = document.querySelector("footer");
   const copyright = document.createElement("p");
-  copyright.innerHTML = `&copy;Oksana Melnyk ${thisYear}`;
+  copyright.innerHTML = `&copy; Oksana Melnyk ${thisYear}`;
+  copyright.classList.add("footer-note");
   footer.appendChild(copyright);
 
   //SKILLS
-  const skills = ["HTML", "CSS", "JavaScript", "Git", "GitHub"];
+  const skills = ["JavaScript", "HTML", "CSS", "Git", "GitHub"];
   const skillsSection = document.getElementById("skills");
   const skillsList = skillsSection.querySelector("ul");
 
   for (let i = 0; i < skills.length; i++) {
     const skill = document.createElement("li");
     skill.textContent = skills[i];
+    skill.classList.add("skill-card");
     skillsList.appendChild(skill);
   }
 
@@ -31,15 +33,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const messageSection = document.getElementById("messages");
     const messageList = messageSection.querySelector("ul");
     const newMessage = document.createElement("li");
-    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a><span> wrote: ${message} </span>`;
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a><span> wrote: &nbsp;&nbsp;&nbsp; ${message} </span>`;
     const removeButton = document.createElement("button");
     removeButton.innerText = "remove";
     removeButton.type = "button";
+    removeButton.classList.add("remove-btn");
     removeButton.addEventListener("click", (e) => {
       const entry = e.target.parentNode;
       entry.remove();
     });
     newMessage.appendChild(removeButton);
+    newMessage.classList.add("message-card");
     messageList.appendChild(newMessage);
     messageForm.reset();
   });
